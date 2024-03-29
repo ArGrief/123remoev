435ce6ae4c87ba497c4bce028e7645c3189fd89f
ArGrief <154673798+ArGrief@users.noreply.github.com>
2024-03-29 13:10:24 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Модерирую BezzLi","Слежу за сообщениями"];
 
 
 let currentIndex = 0;
